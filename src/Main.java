9e782f5a1f6705656f047bb728f13e00ae436a06
Monica <monicacao321@gmail.com>
2023-02-09 21:28:38 -0500

    Database

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,6 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(
-                    "jdbc:mysql://postgres:5432/database",
-                    "postgres",
-                    "postgres1"
-            );
-        } catch (SQLException e) {
-            System.out.println("Unable to connect to the database");
-            e.printStackTrace();
 
-        }
     }
 }
